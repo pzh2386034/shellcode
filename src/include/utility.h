a48f020e8975babb5965c941c86e9782f016b3a2
pzh2386034 <panzehua2386034@gmail.com>
2017-09-07 02:03:08 +0800

    单向链表，留下作业未完成

--- src/include/utility.h
+++ src/include/utility.h
@@ -8,7 +8,29 @@
 extern "C" {
 #endif
 int create_temp_file(const char* filename, char* tmp, size_t len);
+  // 单项链表实现
+typedef char m_temp;
+typedef struct node* link;
+struct node
+{
+    m_temp item;
+    link next;
+};
 
+link make_node(m_temp item);
+void free_node(link p);
+link search(m_temp key);
+void insert(link p);
+void del(link p);
+void traverse(void (*visit)(link));
+void destory(void);
+void push(link p);
+void pop(void);
+  // task
+  // 1. insert函数实现插入排序
+  // 2/ 实现队列的enqueue和dequeue操作。在链表得到末尾再维护一个指针tail, 在tail处enqueue，在head处dequeue
+  // 3. 实现函数void reverse(void), 将链表反转
+  // emd
 #ifdef __cplusplus
 }
 #endif
--- src/utility.c
+++ src/utility.c
@@ -7,6 +7,7 @@
 #include <errno.h>
 #include <string.h>
 
+static link head = NULL;
 int create_temp_file(const char * filename, char * tmp, size_t len)
 {
   int tmpfile ;
@@ -36,4 +37,83 @@ int create_temp_file(const char * filename, char * tmp, size_t len)
   }
   (void)umask(prevmask);
   return OK;
+}
+
+link make_node(m_temp item)
+{
+  link p = malloc(sizeof(*p));
+  p->item = item;
+  p->next = NULL;
+  return p;
+}
+void free_node(link p)
+{
+  free(p);
+}
+link search(m_temp key)
+{
+  link p;
+  for (p = head; p; p->next) {
+    if (p->item == key)
+      {
+        return p;
+      }
+  }
+  return NULL;
+}
+void insert(link p)
+{
+  p->next = head;
+  head = p;
+}
+void del(link p)
+{
+  link pre;
+  if (p == head) {
+    head = p->next;
+    free_node (p);
+  }
+  for (pre =head; pre;pre = pre->next) {
+    if(pre->next == p)
+      {
+        pre->next = p->next;
+        free_node(p);
+        return;
+      }
+  }
+}
+void traverse(void (*visit)(link))
+{
+  link p;
+  for (p = head; p; p->next) {
+    visit(p);
+  }
+}
+void destory(void)
+{
+  link q;
+  link p =head;
+  head=NULL;
+  while (p)
+    {
+      q=p;
+      p = p->next;
+      free_node(p);
+    }
+}
+void push(link p)
+{
+  insert(p);
+}
+link pop(void)
+{
+  if (head == NULL) {
+    return NULL;
+  }
+  else
+    {
+      link p = head;
+      head = head->next;
+      return p;
+    }
 }
